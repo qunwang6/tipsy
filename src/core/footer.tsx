f583c377cc5c236c3949f15f2f9fa90dfbc616b4
qunwang6 <qunwang6@gmail.com>
2023-09-18 09:53:42 +0800

    aaa

--- src/core/footer.tsx
+++ src/core/footer.tsx
@@ -7,14 +7,7 @@ function Footer() {
   if (onHomePage) {
     return (
       <footer className="flex items-center justify-center p-6">
-        <a
-          href="https://github.com/wKovacs64/tipsy"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-current no-underline"
-        >
-          <FaGithub role="img" title="View source on GitHub" size={32} />
-        </a>
+
       </footer>
     );
   }
